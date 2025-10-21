916530a783579fb5def29af172e4fd4d8bc5fa7f
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2025-10-21 11:44:51 +0700

    tower j day

--- Assets/TowerDefenseTK/Scripts/Unit/UnitSO/TowerSO.cs
+++ Assets/TowerDefenseTK/Scripts/Unit/UnitSO/TowerSO.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Tower Data", menuName = "TD Toolkit/Units/Tower")]
 public class TowerSO : UnitSO
 {
+    public enum TowerType
+    {
+        Turret,
+        AoE,
+        Support,
+        Resource
+    }
 
     [Header("Tower Stats")]
     public float fireRate;
     public float range;
     public bool isAoE;
     public float projectileSpeed;
+    public TowerType towerType;
 
     [Tooltip("Gold cost to build this tower.")]
     public int buildCost;
